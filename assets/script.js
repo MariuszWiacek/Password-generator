de720eb2592fcf88d0317ef6acb87f8cd59ef5f2
MariuszWiacek <wiacekm.uber@gmail.com>
2023-01-15 16:35:32 +0000

    var

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-// Array of special characters to be included in password
+
 var specialCharacters = [
   '@',
   '%',
@@ -88,31 +88,10 @@ var upperCasedCharacters = [
   'Z'
 ];
 
-// Function to prompt user for password options
-function getPasswordOptions() {
+// variables 
+var userChoice;
+var passwordLengthSelected = false;
+var passwordArray = [];
+var flatPasswordArray = [];
 
-}
 
-// Function for getting a random element from an array
-function getRandom(arr) {
-
-}
-
-// Function to generate password with user input
-function generatePassword() {
-
-}
-
-// Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector('#password');
-
-  passwordText.value = password;
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
